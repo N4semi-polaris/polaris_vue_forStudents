d0f1c6f3ca83f62d500e568c8d6c3dc2f6093f3a
yuma.maron_1026@icloud.com <yuma.maron_1026@icloud.com>
2020-09-26 17:04:57 +0900

    [add]お気に入りスポット画面作成

---
+++ src/components/Spot_Block.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="SpotBlock">
+        <v-card :color="color" height="auto" class="mx-auto ma-2">
+            <v-container class="pa-1">
+                <v-row class="ma-1">
+                    <v-col cols="8" class="align-self-center pa-1">
+                        <p style="color:white;">{{ name }}</p>
+                    </v-col>
+                    <v-col cols="4" class="align-self-center pa-1">
+                        <p style="color:white;">{{ address }}</p>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-card>
+    </div>
+</template>
+<script>
+export default {
+    props:['name','address'],
+    data:()=>({
+        color: "#210e67",
+    }),
+    methods:{
+    }
+}
+</script>
+
+<style scoped>
+.v-application p {
+    margin-bottom: 0px;
+    font-size: 12px;
+}
+</style>
