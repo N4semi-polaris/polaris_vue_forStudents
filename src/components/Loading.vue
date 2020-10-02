870ba1fe362db9eef1f1eeed4a9f6a37a1c94207
yuma.maron_1026@icloud.com <yuma.maron_1026@icloud.com>
2020-10-02 16:29:00 +0900

    [add]googleloginbtn, loadingCircular

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -1,21 +1,33 @@
 <template>
-    <div id="Loading">
-        <v-row class="justify-center align-center fullview" no-gutters>
-            <v-col class="flex-grow-0">
-                <v-img :src="PolarisLogo" width="250px"/>
-            </v-col>
-        </v-row>
+    <div id="Loading" class="fullview d-flex">
+        <v-container class="align-self-center" no-gutters>
+            <v-row class="justify-center" no-gutters>
+                <v-col class="flex-grow-0">
+                    <v-img :src="PolarisLogo" width="250px"/>
+                </v-col>
+            </v-row>
+            <v-row class="pa-3"><v-col/></v-row>
+            <v-row class="justify-center" no-gutters>
+                <v-col class="flex-grow-0">
+                    <v-img v-if="!isLogined" :src="GoogleLogin" width="200px" />
+                    <v-progress-circular v-else indeterminate color="grey" :size="50" />
+                </v-col>
+            </v-row>
+        </v-container>
     </div>
 </template>
 
 <script>
 import PolarisLogo from "@/assets/Polaris.png"
+import GoogleLogin from "@/assets/btn_google_signin_light.png"
 export default {
   name: 'loading',
   components: {
   },
   data:()=>({
       PolarisLogo: PolarisLogo,
+      GoogleLogin: GoogleLogin,
+      isLogined: false,
   }),
 }
 </script>
