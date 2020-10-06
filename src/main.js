7bde9e4335bae082304048c192b994a7223ecb6b
aasako <g17928ka@gm.tsuda.ac.jp>
2020-10-06 15:29:48 +0900

    vue-touch削除

--- src/main.js
+++ src/main.js
@@ -4,9 +4,6 @@ import router from './router'
 import vuetify from './plugins/vuetify';
 import store from './store'
 
-var VueTouch = require('vue-touch') //New add 10/5
-Vue.use(VueTouch,{name: 'v-touch'}) //New add 10/5
-
 new Vue({
   router,
   vuetify,
