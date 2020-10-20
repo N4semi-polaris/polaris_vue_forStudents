eb826fe8e437e15bbd525b54544fdab7d9d99d57
Pudding <g17914hr@gm.tsuda.ac.jp>
2020-10-20 15:47:24 +0900

    storeにgetter類記載

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,31 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from "vuex-persistedstate";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  plugins: [createPersistedState({
+        key: 'YorimichiApp',
+        paths: ['userEmail',"userAuthCode"],
+        storage: window.sessionStorage
+    })],
   state: {
     userEmail: "",
     userAuthCode: "",
   },
   mutations: {
-    getUserData(state,payload) {
+    setUserData(state,payload) {
       state.userEmail = payload.userEmail
       state.userAuthCode = payload.userAuthCode
+    },
+  },
+  getters: {
+    getUserEmail() {
+      return state.userEmail;
+    },
+    getUserAuthCode() {
+      return state.userAuthCode;
     },
   },
   actions: {
