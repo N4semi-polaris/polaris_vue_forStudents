2c3c6391af89cb682e79265d94df0c1250e57275
Pudding <g17914hr@gm.tsuda.ac.jp>
2020-12-20 01:41:27 +0900

    施設設定のコンポーネント化

---
+++ src/components/dialog/SelectSpots_Gourmet.vue
@@ -0,0 +1,90 @@
+
+<template>
+  <v-dialog v-model="dialog_Gourmet_Open" width="600px">
+    <template>
+      <v-btn color="primary"> 飲食店を選ぶ </v-btn>
+    </template>
+    <v-card>
+      <v-card-title>
+        <h2 class="title mb-2">3つ選択してください</h2>
+      </v-card-title>
+      <v-card-text>
+        <v-chip-group
+          v-model="selectSpot_Gourmet"
+          active-class="primary--text"
+          column
+          multiple
+          max="3"
+        >
+          <v-chip filter v-for="tag in items_Spot_Gourmet" :key="tag" small>
+            {{ tag }}
+          </v-chip>
+        </v-chip-group></v-card-text
+      >
+      <v-card-actions>
+        <v-spacer></v-spacer>
+
+        <v-btn color="green darken-1" text @click="dialog_Gourmet_Open = false">
+          閉じる
+        </v-btn>
+        <v-btn color="green darken-1" text @click="dialog_Gourmet_Open = false">
+          保存
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    dialog_Gourmet_Open: false,
+    selectSpot_Gourmet: [],
+    items_Spot_Gourmet: [
+      "居酒屋",
+      "ラーメン屋",
+      "蕎麦屋",
+      "サンドイッチ、パン屋",
+      "中華料理屋",
+      "スイーツ",
+      "寿司屋",
+      "回転寿司",
+      "とんかつ屋",
+      "洋食屋",
+      "タイ料理",
+      "焼肉",
+      "焼き鳥",
+      "イタリアン",
+      "ハンバーガーショップ",
+      "パスタ",
+      "天ぷら",
+      "カレーライス",
+      "定食屋",
+      "韓国料理屋",
+      "うどん",
+      "鉄板焼き",
+      "オムライス",
+      "おにぎり",
+      "うなぎ",
+      "しゃぶしゃぶ",
+      "お茶漬け",
+      "スープ専門店",
+      "スペイン料理",
+      "お好み焼き",
+      "もんじゃ焼き",
+      "ステーキ",
+      "串カツ、串揚げ",
+      "もつ焼き",
+      "和菓子",
+    ],
+  }),
+  methods: {
+    GdialogOpen() {
+      this.dialog_Gourmet_Open = true;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- src/views/InitialSetting.vue
+++ src/views/InitialSetting.vue
@@ -8,20 +8,15 @@
           <v-row>
             <v-col align="center">
               <v-subheader>好きなスポット</v-subheader>
-              <v-checkbox v-model="spot_1" label="博物館・美術館"></v-checkbox>
-              <v-checkbox v-model="spot_2" label="商業施設"></v-checkbox>
-              <v-checkbox v-model="spot_3" label="レジャー施設"></v-checkbox>
-              <v-checkbox v-model="spot_4" label="神社・寺院"></v-checkbox>
-              <v-checkbox v-model="spot_5" label="カフェ"></v-checkbox>
-
-              <v-select
-                v-model="selected"
-                :items="items_Spot"
-                item-text="category"
-                item-value="name"
-                label="スポット選択"
-              >
-              </v-select>
+              <v-btn color="primary" @click.stop="openGdialog">
+                飲食店を選ぶ
+              </v-btn>
+              <selectSpotGourmet ref="GourmetDialog" />
+              <v-divider></v-divider>
+              <v-btn color="primary" @click.stop="openOdialog">
+                飲食店以外を選ぶ
+              </v-btn>
+              <selectSpotOther ref="OtherDialog" />
             </v-col>
           </v-row>
 
@@ -76,11 +71,15 @@
 
 <script>
 import App_var from "../components/App_bar";
+import selectSpotGourmet from "../components/dialog/SelectSpots_Gourmet.vue";
+import selectSpotOther from "../components/dialog/SelectSpots_Other.vue";
 
 export default {
   name: "InitialSetting",
   components: {
     App_var,
+    selectSpotGourmet,
+    selectSpotOther,
   },
   data: () => ({
     selectTime: [],
@@ -97,101 +96,40 @@ export default {
       selected: 2,
     },
     adress: "",
-    selectSpot: [],
-    items_Spot: [
-      { category: "Cafe", name: "飲食がメインのカフェ・喫茶店" },
-      { category: "Cafe", name: "軽食(パンケーキ、個人経営ファストフード等)" },
-      {
-        category: "Cafe",
-        name: "その他のカフェ(動物カフェ、コンセプトカフェ等)",
-      },
-
-      { category: "Shopping", name: "ドラッグストア" },
-      { category: "Shopping", name: "家電" },
-      { category: "Shopping", name: "携帯電話販売店" },
-      { category: "Shopping", name: "デパート、百貨店" },
-      { category: "Shopping", name: "ショッピングセンター" },
-      { category: "Shopping", name: "アウトレットショップ" },
-      { category: "Shopping", name: "ホームセンター" },
-      { category: "Shopping", name: "商店街" },
-      { category: "Shopping", name: "コンビニ、スーパー" },
-      { category: "Shopping", name: "リサイクル、ディスカウントショップ" },
-      { category: "Shopping", name: "家具屋" },
-      { category: "Shopping", name: "日用雑貨、インテリア用品" },
-      { category: "Shopping", name: "文房具、事務用品" },
-      { category: "Shopping", name: "フラワーショップ" },
-      { category: "Shopping", name: "100円ショップ" },
-      { category: "Shopping", name: "作業服屋" },
-      { category: "Shopping", name: "CD、DVD、ビデオ屋" },
-      { category: "Shopping", name: "書店" },
-      { category: "Shopping", name: "玩具屋" },
-      { category: "Shopping", name: "煙草屋" },
-      {
-        category: "Shopping",
-        name: "アパレルショップ、アクセサリーショップ、時計屋",
-      },
-      { category: "Shopping", name: "食品専門店" },
-      { category: "Shopping", name: "スポーツショップ" },
-
-      { category: "Entertainment", name: "スポーツ施設" },
-      { category: "Entertainment", name: "麻雀" },
-      { category: "Entertainment", name: "ゲームセンター" },
-      { category: "Entertainment", name: "パチンコ、パチスロ" },
-      { category: "Entertainment", name: "ボウリング場" },
-      { category: "Entertainment", name: "ビリヤード" },
-      { category: "Entertainment", name: "遊園地、テーマパーク" },
-      { category: "Entertainment", name: "動物園" },
-      { category: "Entertainment", name: "水族館" },
-      { category: "Entertainment", name: "植物園" },
-      { category: "Entertainment", name: "海水浴場、遊泳場" },
-      { category: "Entertainment", name: "キャンプ場" },
-      { category: "Entertainment", name: "アウトドア施設" },
-      { category: "Entertainment", name: "ボート、ヨット" },
-      { category: "Entertainment", name: "釣り堀、釣り船" },
-      { category: "Entertainment", name: "プール" },
-      { category: "Entertainment", name: "観光農園" },
-      { category: "Entertainment", name: "映画館" },
-      { category: "Entertainment", name: "美術館" },
-      { category: "Entertainment", name: "画廊" },
-      { category: "Entertainment", name: "博物館、科学館" },
-      { category: "Entertainment", name: "コンサートホール" },
-      { category: "Entertainment", name: "ライブハウス" },
-      { category: "Entertainment", name: "公園" },
-      { category: "Entertainment", name: "道の駅" },
-      { category: "Entertainment", name: "パーキングエリア" },
-      { category: "Entertainment", name: "ドライブイン" },
-
-      { category: "Living", name: "マッサージ、整体、治療院" },
-      { category: "Living", name: "ベビー用品、子供服" },
-      { category: "Living", name: "郵便、郵便局" },
-      { category: "Living", name: "宅配便" },
-      { category: "Living", name: "コインランドリー" },
-      { category: "Living", name: "レンタルCD、DVD、ビデオ" },
-      { category: "Living", name: "レンタルサイクル" },
-      { category: "Living", name: "ガソリンスタンド" },
-      { category: "Living", name: "靴修理" },
-      { category: "Living", name: "クリーニング" },
-      { category: "Living", name: "質屋" },
-      { category: "Living", name: "洗車場" },
-      { category: "Living", name: "銀行" },
-      { category: "Living", name: "ペットショップ、動物病院" },
-      { category: "Living", name: "銭湯" },
-      { category: "Living", name: "スーパー銭湯" },
-      { category: "Living", name: "健康ランド" },
-      { category: "Living", name: "温泉浴場" },
-      { category: "Living", name: "岩盤浴" },
-      { category: "Living", name: "サウナ" },
-      { category: "Living", name: "官公庁" },
-      { category: "Living", name: "美容院、サロン" },
-      { category: "Living", name: "寺院、神社、教会" },
-    ],
   }),
-
+  computed: {
+    allSelected() {
+      return this.selected.length === this.items.length;
+    },
+    categories() {
+      const search = this.search.toLowerCase();
+      if (!search) return this.items;
+      return this.items.filter((item) => {
+        const text = item.text.toLowerCase();
+        return text.indexOf(search) > -1;
+      });
+    },
+    selections() {
+      const selections = [];
+      for (const selection of this.selected) {
+        selections.push(selection);
+      }
+      return selections;
+    },
+  },
+  watch: {
+    /*selected() {
+      this.search = "";
+    },*/
+  },
   mounted() {},
   methods: {
-    /*showSelectedSpotItem: function () {
-      return items_Spot[this.selectedSpot];
-    },*/
+    openGdialog() {
+      this.$refs.GourmetDialog.GdialogOpen();
+    },
+    openOdialog() {
+      this.$refs.OtherDialog.OdialogOpen();
+    },
 
     /*postSettingData() {
       //作っている最中
