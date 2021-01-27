2c632e685372d698137b269138d7358ad896a95b
yuma.maron_1026@icloud.com <yuma.maron_1026@icloud.com>
2021-01-28 05:59:42 +0900

    []

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -53,7 +53,7 @@ export default {
         if (authCode === null) return;
         let data = new URLSearchParams();
         data.append("code", authCode);
-        this.$axios.post("polaris_vue_app/accounts/google/signup/", data).then(
+        this.$axios.post("http://17930ky.pythonanywhere.com/accounts/google/signup/", data).then(
           (response) => {
             response.data.authCode = authCode;
             //console.log(response.data);
@@ -62,7 +62,6 @@ export default {
           },
           (error) => {
             console.error("[error, axios]サインインに失敗: " + error);
-            console.dir(error)
             return null;
           }
         );
