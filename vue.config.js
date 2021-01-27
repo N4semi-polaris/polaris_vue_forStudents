ac33962239b85d1f3058f7bc560739b26937fea6
yuma.maron_1026@icloud.com <yuma.maron_1026@icloud.com>
2021-01-28 00:19:34 +0900

    [add]for deploy

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,21 @@
 module.exports = {
+  publicPath: '/polaris_vue_app/',
+  outputDir: 'docs',
   "transpileDependencies": [
     "vuetify"
   ],
   devServer: {
     proxy: {
       "/accounts/": {
-        target: "http://localhost:8000",
+        target: "http://17930ky.pythonanywhere.com",
         changeOrigin: true,
       },
       "/calendar/":{
-        target: "http://localhost:8000",
+        target: "http://17930ky.pythonanywhere.com",
         changeOrigen: true,
       },
       "/recommend/":{
-        target: "http://localhost:8000",
+        target: "http://17930ky.pythonanywhere.com",
         changeOrigen: true,
       },
     }
